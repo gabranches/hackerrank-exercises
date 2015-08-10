eb074033d074120a8af61088b202caad78c2bac1
Gustavo Abranches <gabranches@UM-L0R6G3QN.local>
2015-08-10 12:14:33 -0400

    first commit

---
+++ lists/__main__.py
@@ -0,0 +1,35 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+L = []
+
+while True:
+    try:
+        n = int(raw_input())
+    except ValueError:
+        print 'Invalid integer, please try again'
+    else:
+        break
+
+def scan(list):
+    cmd = list[0]
+    if cmd == 'insert':
+        L.insert(int(list[1]), int(list[2]))
+    elif cmd == 'remove':
+        L.remove(int(list[1]))
+    elif cmd == 'append':
+        L.append(int(list[1]))
+    elif cmd == 'pop':
+        L.pop()
+    elif cmd == 'reverse':
+        L.reverse()
+    elif cmd == 'sort':
+        L.sort()
+    elif cmd == 'print':
+        print L
+    else:
+        print 'Inavlid command, please try again'
+        scan(raw_input().split(' '))
+
+    
+for i in range(0, n):
+    commands = raw_input().split(' ')
+    scan(commands)
---
+++ sets/__main__.py
@@ -0,0 +1,21 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def build_set():
+    a = int(raw_input())
+    a_str = raw_input()
+    a_list = map(int, a_str.split())
+    a_set = set(a_list)
+    return a_set
+
+one = build_set()
+two = build_set()
+
+diff_one = one.difference(two)
+diff_two = two.difference(one)
+
+diff = diff_one.union(diff_two)
+
+diff_list = [x for x in diff]
+diff_list = sorted(diff_list)
+
+for i in diff_list:
+    print i
