4362ad6a7ab2c3b90ded88eeb06593e488193079
Gustavo Abranches <gabranches@UM-L0R6G3QN.local>
2015-08-11 15:58:21 -0400

    added fibbonaci and validate email

---
+++ functionals_fibbonaci/__main__.py
@@ -0,0 +1,17 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(raw_input())
+
+fib_list = []
+
+for i in range(0, n):
+	if i == 0:
+		fib_list.append(i)
+	elif i == 1:
+		fib_list.append(i)
+	else:
+		fib_list.append(fib_list[i-1] + fib_list[i-2])
+
+cube = lambda x: x ** 3
+
+print map(cube, fib_list)
---
+++ functionals_validate_email/__main__.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+import re
+
+emails = []
+valid_emails = []
+
+n = int(raw_input())
+
+for _ in range(0, n):
+	emails.append(raw_input())
+
+for address in emails:
+    p = re.compile(r'^[-_a-z0-9]+@[a-z0-9]+\.\w{1,3}$', re.I)
+    m = p.match(address)
+    if m:
+        valid_emails.append(address)
+
+print sorted(valid_emails)
