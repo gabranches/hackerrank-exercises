bc626ca1baac2acdd2a8d39cda21ba0c84aabc4f
Gustavo Abranches <gabranches@UM-L0R6G3QN.local>
2015-08-11 16:43:50 -0400

    added decorators_mobile_number

--- classes_complex_numbers/__main__.py
+++ classes_complex_numbers/__main__.py
@@ -1,5 +1,3 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-
 class Complex(object):
 
 	def __init__(self, real, complex):
---
+++ decorators_mobile_number/__main__.py
@@ -0,0 +1,20 @@
+n = int(raw_input())
+numbers = []
+
+for _ in range(0, n):
+	numbers.append(raw_input())
+
+def number_standardizer(func):
+
+	def add_prefix(*args, **kwargs):
+		return [ "+91 " + x[:5] + ' ' + x[-5:] for x in func(*args, **kwargs) ]
+
+	return add_prefix
+
+
+@number_standardizer
+def sort_numbers(numbers):
+	return sorted([x[-10:] for x in numbers])
+
+for num in sort_numbers(numbers):
+	print num
