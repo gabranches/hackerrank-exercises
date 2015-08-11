14776125b54f85023254aa95f8a11c34aacb8781
Gustavo Abranches <gabranches@UM-L0R6G3QN.local>
2015-08-11 14:36:04 -0400

    fixed complex numbers class

--- classes_complex_numbers/__main__.py
+++ classes_complex_numbers/__main__.py
@@ -3,46 +3,54 @@
 class Complex(object):
 
 	def __init__(self, real, complex):
-		self.real = real
-		self.complex = complex
+		self.real = float(real)
+		self.complex = float(complex)
 
 	def __add__(n1, n2):
+		'''Adds two complex numbers'''
 		return Complex(n1.real + n2.real, n1.complex + n2.complex)
 
 	def __sub__(n1, n2):
+		'''Subtracts two complex numbers'''
 		return Complex(n1.real - n2.real, n1.complex - n2.complex)
 
 	def __mul__(n1, n2):
+		'''Multiplies two complex numbers'''
 		return Complex(n1.real * n2.real - n1.complex * n2.complex, 
 				n1.real * n2.complex + n2.real * n1.complex)
 
 	def __div__(n1, n2):
+		'''Divides two complex numbers'''
 		conj = n2.conjugate()
 		numerator = n1 * conj
 		denominator = float((n2 * conj).real)
 		return Complex(numerator.real / denominator, numerator.complex / denominator)
 
+	def fmt(self, n):
+		'''Format complex number output'''
+		return "{0:.2f}".format(round(n, 2))	
+
 	def __str__(self):
 		if self.complex == float(0):
-			return format_imaginary(self.real)
+			return self.fmt(self.real)
 		elif self.real == float(0):
-			return format_imaginary(self.complex) + 'i'
+			return self.fmt(self.complex) + 'i'
 		elif self.complex > 0:
-			return  format_imaginary(self.real) + " + " + format_imaginary(self.complex) + 'i'
+			return self.fmt(self.real) + " + " + self.fmt(self.complex) + 'i'
 		else:
-			return format_imaginary(self.real) + " - " + format_imaginary(abs(self.complex))+ 'i'
-
+			return self.fmt(self.real) + " - " + "{0:.2f}".format(round(abs(self.complex), 2)) + 'i'
+	
 	def conjugate(self):
-			return Complex(self.real, -self.complex)
+		'''Conjugates a complex number'''
+		return Complex(self.real, -self.complex)
 
-def format_imaginary(s):
-	return "{0:.2f}".format(round(float(s), 2))
+	def mod(self):
+		'''Returns the modulus of a complex number'''
+		return Complex(((self.real ** 2 + self.complex ** 2) ** 0.5), 0)
 
-def mod(n):
-	return Complex(((n.real ** 2 + n.complex ** 2) ** 0.5), 0)
 
-n1 = map(float, raw_input().split())
-n2 = map(float, raw_input().split())
+n1 = raw_input().strip().split()
+n2 = raw_input().strip().split()
 
 n1 = Complex(n1[0], n1[1])
 n2 = Complex(n2[0], n2[1])
@@ -51,5 +59,5 @@ print n1 + n2
 print n1 - n2
 print n1 * n2
 print n1 / n2
-print mod(n1)
-print mod(n2)
+print n1.mod()
+print n2.mod()
